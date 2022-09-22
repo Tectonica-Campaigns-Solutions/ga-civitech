568e062e38332f80dad02c603473d62c8f29bb38
marcelsafont <marcel.safont1983@gmail.com>
2022-09-22 11:01:47 +0200

    better naming

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
 import Blocks from "../components/Blocks"
 
 const Page = ({ data: { page } }) => {
