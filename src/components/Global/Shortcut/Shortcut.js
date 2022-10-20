76b62b2a111b1d613b6bae1c4c017f6bbd29405f
Rodrigo Leon <rodrigo@tectonica.co>
2022-10-20 11:12:09 -0300

    Shortcuts hover style added.

--- src/components/Global/Shortcut/Shortcut.js
+++ src/components/Global/Shortcut/Shortcut.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import arrow1 from "../../Icons/arrow1.svg";
+import arrow2 from "../../Icons/arrow2.svg";
+import arrow3 from "../../Icons/arrow3.svg";
+
 import "./index.scss";
 
 export default function Shortcut({ title, description, btnLabel, btnUrl }) {
@@ -8,8 +12,14 @@ export default function Shortcut({ title, description, btnLabel, btnUrl }) {
         <div className="shortcut">
             <h4>{title}</h4>
             <p>{description}</p>
+            <Link to={btnUrl}>{btnLabel} ›</Link>
 
-            <Link to={btnUrl}>{btnLabel}</Link>
+            {/* Arrow icons */}
+            <div className='arrows'>
+                <img src={arrow1} width="49" />
+                <img src={arrow2} width="38" />
+                <img src={arrow3} width="25" />
+            </div>
         </div>
     )
 }
