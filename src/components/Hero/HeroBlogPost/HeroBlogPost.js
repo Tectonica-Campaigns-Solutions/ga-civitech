5dcd30e6f9e3f721570024f42609b29b719a396a
Rodrigo Leon <rodrigo@tectonica.co>
2022-11-17 11:54:34 -0300

    Blog post detail: initial layout, WIP...

---
+++ src/components/Hero/HeroBlogPost/HeroBlogPost.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Tag from '../../Global/Tag/Tag';
+import ImageWrapper from '../../Slider/ImageWrapper';
+
+import './index.scss';
+
+export default function HeroBlogPost({ title, summary, createdAt, topic, image }) {
+  return (
+    <div className="hero-blog-post">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6">
+            <ImageWrapper image={image} />
+          </div>
+
+          <div className="col-lg-6 content">
+            <div className="info">
+              <span className="date">{createdAt}</span>
+              <Tag title={topic ? topic.name : ''} />
+            </div>
+
+            <h2>{title}</h2>
+            <p>{summary}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
