bb1dd50123799b7da89f9ef96267ed4910694101
Rodrigo Leon <rodrigo@tectonica.co>
2022-11-18 13:10:43 -0300

    Related products: Accordion first approach added.

---
+++ src/components/Global/Accordion/Accordion.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import AccordionItem from './AccordionItem';
+
+const Accordion = ({ content }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleOnClickTab = indexTab => {
+    setActiveTab(indexTab === activeTab ? -1 : indexTab);
+  };
+
+  return (
+    <div>
+      {content.map(({ title, descriptionPreview, imagePreview }, index) => (
+        <AccordionItem
+          title={title}
+          content={descriptionPreview}
+          image={imagePreview}
+          isActive={activeTab === index}
+          handleOnClickTab={() => handleOnClickTab(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
---
+++ src/components/Global/Accordion/AccordionItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import GlobalImage from '../GlobalImage/GlobalImage';
+import openTab from '../../Icons/tab-plus.svg';
+import closeTab from '../../Icons/tab-minus.svg';
+
+import './index.scss';
+
+const AccordionItem = ({ title, content, image, isActive, handleOnClickTab }) => {
+  return (
+    <div className={`cv-accordion-item ${isActive ? 'active' : ''}`}>
+      <div className="btn-item" onClick={handleOnClickTab}>
+        <img src={isActive ? closeTab : openTab} />
+      </div>
+
+      <div className="accordion-content">
+        <div className="row">
+          <div className="col-lg-6">
+            <h2 onClick={handleOnClickTab}>{title}</h2>
+            {isActive && <div className="content" dangerouslySetInnerHTML={{ __html: content }} />}
+          </div>
+
+          <div className="col-lg-5 col-md-12 offset-lg-1 image">{isActive && <GlobalImage image={{ ...image }} />}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionItem;
