d138df036deb5a089c7eb3a068055aa908fe6dc4
Rodrigo Leon <rodrigo@tectonica.co>
2022-11-18 13:23:08 -0300

    Log added

--- src/components/Global/Accordion/Accordion.js
+++ src/components/Global/Accordion/Accordion.js
@@ -3,6 +3,7 @@ import AccordionItem from './AccordionItem';
 
 const Accordion = ({ content }) => {
   const [activeTab, setActiveTab] = useState(0);
+  console.log('Force new commit');
 
   const handleOnClickTab = indexTab => {
     setActiveTab(indexTab === activeTab ? -1 : indexTab);
