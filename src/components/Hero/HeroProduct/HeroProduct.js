e369b51e9d00aa53fecb199f967b0ee2d30ca170
marcelsafont <marcel.safont1983@gmail.com>
2022-11-19 17:54:41 +0100

    add chek to image prop heroproduct

--- src/components/Hero/HeroProduct/HeroProduct.js
+++ src/components/Hero/HeroProduct/HeroProduct.js
@@ -4,12 +4,13 @@ import NarrativeBlock from '../../Blocks/NarrativeBlock/NarrativeBlock';
 import './index.scss';
 
 export default function HeroProduct({ data }) {
+  // convert image object to array to pass to narrativeblock
   const productBlockData = {
     backgroundColor: data.backgroundColor,
     alignment: data.alignment,
     title: data.title,
     textContent: data.description,
-    image: [data.image],
+    image: data.image ? [data.image] : null,
     ctas: data.ctas,
     logo: data.logo,
   };
