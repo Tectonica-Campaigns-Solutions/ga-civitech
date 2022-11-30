098583d8b369b9e1aeaaa2540989b861e5a49f88
Rodrigo Leon <rodrigo@tectonica.co>
2022-11-30 12:34:11 -0300

    Blog list cta added

--- src/components/Blocks/BlogPost/BlogPostTab.js
+++ src/components/Blocks/BlogPost/BlogPostTab.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import BlogPostCard from '../../Global/BlogPostCard/BlogPostCard';
+import Cta from '../../Global/Cta/Cta';
 
 function BlogPostTab({ title, items }) {
+  const shouldRenderCta = items.length >= 12;
+
   return (
     <>
       <h2>{title}</h2>
 
       <div className="row gy-5">
-        {items.map(item => {
-          return (
-            <div className="col-lg-4">
+        {items.map((item, index) => (
+          <>
+            <div className="col-lg-4" key={index}>
               <BlogPostCard
                 slug={item.slug}
                 model={item.model}
@@ -19,8 +22,25 @@ function BlogPostTab({ title, items }) {
                 title={item.title}
               />
             </div>
-          );
-        })}
+
+            {shouldRenderCta && index === 5 && (
+              <div className="col-lg-12 blog-post-tab-cta">
+                <div className="row align-items-center">
+                  <div className="col-lg-9">
+                    <h4>
+                      Lorem ipsum dolor sit amet consectetur. Sed egestas dictumst nunc eget feugiat arcu sed sapien.
+                      Lacus ut venenatis tortor amet.
+                    </h4>
+                  </div>
+
+                  <div className="col-lg-3">
+                    <Cta label="Learn more" url="/" isButton />
+                  </div>
+                </div>
+              </div>
+            )}
+          </>
+        ))}
       </div>
     </>
   );
