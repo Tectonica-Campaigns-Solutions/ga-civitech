493588ad0d45b6b0f24dd5058ec35430a12ebd61
Rodrigo Leon <rodrigo@tectonica.co>
2022-12-16 11:20:03 -0300

    Top message fixes.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
 import TopMessage from './Global/TopMessage/TopMessage';
 import Header from './Header';
 import Footer from './Global/Footer/Footer';
@@ -15,19 +7,9 @@ import Footer from './Global/Footer/Footer';
 import '../styles/main.scss';
 
 const Layout = ({ location, children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <>
-      {/* <TopMessage /> */}
+      <TopMessage />
       <Header location={location} />
       <main>{children}</main>
       <Footer />
