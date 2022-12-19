c81c3993d58a8c4ada214c967f7be45dc3484d23
Rodrigo Leon <rodrigo@tectonica.co>
2022-12-19 08:52:59 -0300

    Fix

--- src/components/Global/Link.js
+++ src/components/Global/Link.js
@@ -7,13 +7,13 @@ export default class Link extends React.Component {
     //check for string in case link recieve hardoced link on templates ex: logo
     if (to.content?.slug || typeof to === 'string') {
       return (
-        <GatsbyLink to={`${to.content?.slug ? '/' + to.content?.slug : to}`} {...rest}>
+        <GatsbyLink to={`${to?.content?.slug ? '/' + to?.content?.slug : to}`} {...rest}>
           {children}
         </GatsbyLink>
       );
     } else {
       return (
-        <a href={to.url} {...rest}>
+        <a href={to?.url} {...rest}>
           {children}
         </a>
       );
