3247513428b981f966e30f4a262a1e73c43169d0
Rodrigo Leon <rodrigo@tectonica.co>
2022-12-19 08:56:20 -0300

    Fix

--- src/components/Global/Link.js
+++ src/components/Global/Link.js
@@ -5,7 +5,7 @@ export default class Link extends React.Component {
   render() {
     const { to, children, ...rest } = this.props;
     //check for string in case link recieve hardoced link on templates ex: logo
-    if (to.content?.slug || typeof to === 'string') {
+    if (to?.content?.slug || typeof to === 'string') {
       return (
         <GatsbyLink to={`${to?.content?.slug ? '/' + to?.content?.slug : to}`} {...rest}>
           {children}
