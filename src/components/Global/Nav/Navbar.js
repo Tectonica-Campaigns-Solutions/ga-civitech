2ac686133aba75efb1cd2768e7f747b6b05ff090
Rodrigo Leon <rodrigo@tectonica.co>
2023-01-11 19:25:48 -0300

    More styles fixes

--- src/components/Global/Nav/Navbar.js
+++ src/components/Global/Nav/Navbar.js
@@ -33,62 +33,60 @@ const Navbar = ({ navData, path }) => {
   }, []);
 
   return (
-    <>
-      <div>
-        <nav
-          className={`navbar navbar-expand-lg ${isHomePage ? 'home-nav' : ''} ${expanded ? 'expanded' : ''} ${
-            scrollPosition > 40 ? 'sticky-nav' : ''
-          }`}
+    <div className="navbar-container">
+      <nav
+        className={`navbar navbar-expand-lg ${isHomePage ? 'home-nav' : ''} ${expanded ? 'expanded' : ''} ${
+          scrollPosition > 40 ? 'sticky-nav' : ''
+        }`}
+      >
+        <Link className="navbar-brand" to={'/'}>
+          <img src={logo} alt="logo civitech" />
+        </Link>
+
+        <button
+          type="button"
+          data-target="#navNav"
+          aria-expanded="false"
+          aria-controls="navNav"
+          data-toggle="collapse"
+          className="navbar-toggler"
+          aria-label="Toggle navigation"
+          onClick={() => setExpanded(prev => !prev)}
         >
-          <Link className="navbar-brand" to={'/'}>
-            <img src={logo} alt="logo civitech" />
-          </Link>
+          <span className={`${expanded ? 'open-toggle ' : ''} navbar-toggler-icon`}></span>
+        </button>
 
-          <button
-            type="button"
-            data-target="#navNav"
-            aria-expanded="false"
-            aria-controls="navNav"
-            data-toggle="collapse"
-            className="navbar-toggler"
-            aria-label="Toggle navigation"
-            onClick={() => setExpanded(prev => !prev)}
-          >
-            <span className={`${expanded ? 'open-toggle ' : ''} navbar-toggler-icon`}></span>
-          </button>
-
-          <div className={` ${expanded ? 'show' : ''} collapse navbar-collapse`} id="navNav">
-            <ul className="navbar-nav mr-auto nav-c-group-items">
-              {navigationItems?.map(link => {
-                // If the link has children or is a mega menu we do not need to redirect to another page
-                if (isArray(link.links) || !!link.megaMenu) {
-                  return (
-                    <span
-                      onClick={() => setActiveLink(link)}
-                      className={`nav-c-item ${activeLink === link ? 'active' : ''}`}
-                    >
-                      {link.label}
-
-                      <span>
-                        <img src={activeLink === link ? dropdownActiveIcon : dropdownIcon} />
-                      </span>
-                    </span>
-                  );
-                }
-
+        <div className={` ${expanded ? 'show' : ''} collapse navbar-collapse`} id="navNav">
+          <ul className="navbar-nav mr-auto nav-c-group-items">
+            {navigationItems?.map(link => {
+              // If the link has children or is a mega menu we do not need to redirect to another page
+              if (isArray(link.links) || !!link.megaMenu) {
                 return (
-                  <Link to={link} className={link.isButton ? 'btn btn-primary' : ''}>
+                  <span
+                    onClick={() => setActiveLink(link)}
+                    className={`nav-c-item ${activeLink === link ? 'active' : ''}`}
+                  >
                     {link.label}
-                  </Link>
-                );
-              })}
-            </ul>
-          </div>
-        </nav>
 
-        {activeLink && <MegaMenu link={activeLink} />}
-      </div>
-    </>
+                    <span>
+                      <img src={activeLink === link ? dropdownActiveIcon : dropdownIcon} />
+                    </span>
+                  </span>
+                );
+              }
+
+              return (
+                <Link to={link} className={link.isButton ? 'btn btn-primary' : ''}>
+                  {link.label}
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+
+      {activeLink && <MegaMenu link={activeLink} />}
+    </div>
   );
 };
 
