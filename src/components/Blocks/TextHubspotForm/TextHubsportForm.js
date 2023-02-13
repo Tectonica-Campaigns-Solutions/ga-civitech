70399c8df81fc2340552273ba34420f64a43574b
Rodrigo Leon <rodrigo@tectonica.co>
2023-02-13 11:26:45 -0300

    Hubspot form: Basic styles for two columns layout

--- src/components/Blocks/TextHubspotForm/TextHubsportForm.js
+++ src/components/Blocks/TextHubspotForm/TextHubsportForm.js
@@ -59,7 +59,11 @@ function TextHubspotForm({ block }) {
               {titleInsideBox && <h3>{titleInsideBox}</h3>}
               {descriptionInsideBox && <p>{descriptionInsideBox}</p>}
 
-              <div id={`hubspotForm-${block.id}`} />
+              {/* <div id={`hubspotForm-${block.id}`} /> */}
+
+              <div className="hubspot-form-block">
+                <HubspotHardcodedTest />
+              </div>
             </div>
           </div>
         </div>
@@ -83,3 +87,97 @@ function TextHubspotForm({ block }) {
 }
 
 export default TextHubspotForm;
+
+// TODO: Remove, just for testing styles
+const HubspotHardcodedTest = () => (
+  <div id="hubspotForm-DatoCmsHubspot-85721204" data-hs-forms-root="true">
+    <form
+      id="hsForm_3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+      method="POST"
+      accept-charset="UTF-8"
+      enctype="multipart/form-data"
+      novalidate=""
+      action="https://forms-eu1.hsforms.com/submissions/v3/public/submit/formsnext/multipart/26289884/3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+      className="hs-form-private hsForm_3eb4259d-97a6-4a8c-af05-330d2089f7ca hs-form-3eb4259d-97a6-4a8c-af05-330d2089f7ca hs-form-3eb4259d-97a6-4a8c-af05-330d2089f7ca_082fa578-1e4b-4b51-8eca-3d4dde01bf18 hs-form stacked"
+      target="target_iframe_3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+      data-instance-id="082fa578-1e4b-4b51-8eca-3d4dde01bf18"
+      data-form-id="3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+      data-portal-id="26289884"
+    >
+      <fieldset className="form-columns-2">
+        <div className="hs_email hs-email hs-fieldtype-text field hs-form-field">
+          <label
+            id="label-email-3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+            className=""
+            placeholder="Enter your Correo"
+            for="email-3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+          >
+            <span>Correo</span>
+            <span className="hs-form-required">*</span>
+          </label>
+          <legend
+            className="hs-field-desc"
+            // style="display: none;"
+          ></legend>
+          <div className="input">
+            <input
+              id="email-3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+              name="email"
+              required=""
+              placeholder="Email"
+              type="email"
+              className="hs-input"
+              inputmode="email"
+              autocomplete="email"
+              value=""
+            />
+            <div
+              data-lastpass-icon-root="true"
+              // style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;"
+            ></div>
+          </div>
+        </div>
+        <div className="hs_lastname hs-lastname hs-fieldtype-text field hs-form-field">
+          <label
+            id="label-lastname-3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+            className=""
+            placeholder="Enter your Apellido"
+            for="lastname-3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+          >
+            <span>Apellido</span>
+          </label>
+          <legend
+            className="hs-field-desc"
+            //style="display: none;"
+          ></legend>
+          <div className="input">
+            <input
+              id="lastname-3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+              name="lastname"
+              placeholder="lastname"
+              type="text"
+              className="hs-input"
+              inputmode="text"
+              autocomplete="family-name"
+              value=""
+            />
+          </div>
+        </div>
+      </fieldset>
+      <div className="hs_submit hs-submit">
+        <div
+          className="hs-field-desc"
+          //style="display: none;"
+        ></div>
+        <div className="actions">
+          <input type="submit" className="hs-button primary large" value="Enviar" />
+        </div>
+      </div>
+
+      <iframe
+        name="target_iframe_3eb4259d-97a6-4a8c-af05-330d2089f7ca"
+        // style="display: none;"
+      ></iframe>
+    </form>
+  </div>
+);
