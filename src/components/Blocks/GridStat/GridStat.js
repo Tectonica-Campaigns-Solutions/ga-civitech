095a786fb32324c5a5aa5766f0f1936a44cfae08
Rodrigo Leon <rodrigo@tectonica.co>
2023-02-17 08:56:35 -0300

    Small fix

--- src/components/Blocks/GridStat/GridStat.js
+++ src/components/Blocks/GridStat/GridStat.js
@@ -25,11 +25,9 @@ function GridStat({ block }) {
                 className={`${isGridSizeGreaterThan3 ? 'col-lg-3' : 'col-lg-4'} col-md-4 col-sm-6 stat-item`}
                 key={index}
               >
-                {item.icon && (
-                  <div className={`stat-image ${isGridSizeGreaterThan3 ? 'sm-img' : 'lg-img'}`}>
-                    <GlobalImage image={item.icon} />
-                  </div>
-                )}
+                <div className={`stat-image ${isGridSizeGreaterThan3 ? 'sm-img' : 'lg-img'}`}>
+                  <GlobalImage image={item.icon} />
+                </div>
 
                 {item.title && <h3 className={`${isGridSizeGreaterThan3 ? 'sm-title' : 'lg-title'}`}>{item.title}</h3>}
                 {item.text && <span className={`${isGridSizeGreaterThan3 ? 'sm-text' : 'lg-text'}`}>{item.text}</span>}
