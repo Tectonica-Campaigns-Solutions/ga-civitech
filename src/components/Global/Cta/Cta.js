93d9d1a50bf7bdfebb12cdf6df8616497e7a252c
Rodrigo Leon <rodrigo@tectonica.co>
2023-03-02 09:23:07 -0300

    Fix cta external links urls

--- src/components/Global/Cta/Cta.js
+++ src/components/Global/Cta/Cta.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Link from '../Link/Link';
-import { getCtaTitle, getCtaUrl } from '../../../utils';
+import { getCtaTitle } from '../../../utils';
 
 export default function Cta({ cta = null, url = '', label = '', isButton = false }) {
   const ctaTitle = cta ? getCtaTitle(cta) : label;
-  const ctaUrl = cta ? getCtaUrl(cta) : url;
   const isCtaPrimaryButton = cta?.isButton || isButton;
 
   return (
     <div>
-      <Link className={`btn ${isCtaPrimaryButton ? 'btn-primary' : ''}`} to={ctaUrl}>
+      <Link className={`btn ${isCtaPrimaryButton ? 'btn-primary' : ''}`} to={cta || url}>
         {ctaTitle}
       </Link>
     </div>
